045340fbe74445c8c34f490ddeb71cb4ed38e01e
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-08-19 13:30:49 +0200

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -3,16 +3,16 @@
 
 LiquidCrystal lcd(21, 19, 4, 0, 2, 15);
 
-const int humidityPin = 13;
+const int humidityPin = 22;
 int temperaturePin = 35;
 
-const int buttonPin = 34;  
+const int buttonPin = 17;  
 bool lastButtonState = HIGH;
 bool currentButtonState = HIGH;
-unsigned long lastDebounceTime = 0;  
-const unsigned long debounceDelay = 50;  
+unsigned long lastDebounceTime = 0;    
 unsigned long buttonPressTime = 0;
-const unsigned long displayDuration = 10000;  
+const unsigned long displayTime = 10000;  
+bool buttonState;
 bool displayOn = false; 
 
 DHTesp dhtSensor;
@@ -20,11 +20,28 @@ void setup() {
   lcd.begin(16,2);
   dhtSensor.setup(humidityPin, DHTesp::DHT22); 
   Serial.begin(115200);
-  Serial.println("Hello, ESP32!");
+  pinMode(humidityPin, INPUT_PULLUP);
 }
 
 void loop() {
-  TempAndHumidity  data = dhtSensor.getTempAndHumidity();
+   if (dhtSensor.getStatus() != DHTesp::ERROR_NONE) {
+    Serial.println("Failed to read from DHT sensor!");
+  }
+  if (buttonState != lastButtonState){
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > displayTime) {
+
+    if (buttonState != currentButtonState) {
+      currentButtonState = buttonState;
+    
+   if (currentButtonState == LOW) {
+    Serial.print("pressed");
+
+  TempAndHumidity data = dhtSensor.getTempAndHumidity();
   Serial.println(String(data.temperature, 2) + "°C");
-  
+   }
+  }
+ }
+ delay(500);
 }
